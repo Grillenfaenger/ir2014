1ec23e51a89616c12024e0eaad5b26dc76bff909
claesn <c.neuefeind@uni-koeln.de>
2014-10-23 18:01:15 +0200

    Material für die zweite Praxissitzung

---
+++ IR-Praxis/src/ir2/LinearSearch.java
@@ -0,0 +1,25 @@
+package ir2;
+
+import ir1.Corpus;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinearSearch implements InformationRetrieval{
+
+	public LinearSearch(Corpus corpus) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public Set<Integer> search(String query) {
+
+		Set<Integer> result = new HashSet<Integer>();
+
+		//TODO: Suche implementieren
+		
+		return result;
+	}
+
+}
---
+++ IR-Praxis/src/ir2/TermDokumentMatrix.java
@@ -0,0 +1,19 @@
+package ir2;
+
+import java.util.Set;
+
+import ir1.Corpus;
+
+public class TermDokumentMatrix implements InformationRetrieval {
+
+	public TermDokumentMatrix(Corpus corpus) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Set<Integer> search(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
