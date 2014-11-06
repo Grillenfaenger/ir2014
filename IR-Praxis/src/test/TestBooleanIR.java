ec0f157461843feffc44f3a28ed8925a466137bf
claesn <c.neuefeind@uni-koeln.de>
2014-11-06 10:23:30 +0100

    Zusätzliche Vergleiche von Testabfragen.

--- IR-Praxis/src/test/TestBooleanIR.java
+++ IR-Praxis/src/test/TestBooleanIR.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 import ir1.Corpus;
 import ir2.LinearSearch;
 import ir2.TermDokumentMatrix;
+import ir3.InvertedIndex;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ import org.junit.Test;
 public class TestBooleanIR {
 
 	private static Corpus corpus;
+	private static String query;
 
 	@BeforeClass
 	public static void setUp() throws Exception {
@@ -38,28 +40,75 @@ public class TestBooleanIR {
 	@Test
 	public void testLinearSearch() {
 		// Testen, ob lineare Suche ein Ergebnis liefert:
-		
+
+		System.out.println();
+		System.out.println("Lineare Suche:");
+		System.out.println("-------------------");
 		LinearSearch linear = new LinearSearch(corpus);
 
-		String query = "Brutus";
+		query = "Brutus";
 		Set<Integer> result = linear.search(query);
-		assertTrue("Mindestens ein Treffer erwartet", result.size() >= 1);
 		System.out.println("Ergebnis für " + query + ": " + result);
+		assertTrue("Mindestens ein Treffer erwartet", result.size() >= 1);
 
 		query = "Brutus Caesar";
 		Set<Integer> result2 = linear.search(query);
+		System.out.println("Ergebnis für " + query + ": " + result2);
 		assertTrue("Ergebnis-Set sollte größer sein als bei einzelnem Term",
 				result2.size() >= result.size());
-		System.out.println("Ergebnis für " + query + ": " + result2);
 	}
 
 	@Test
 	public void testMatrixSearch() {
 		// Testen, ob Suche in Term-Dokument-Matrix ein Ergebnis liefert:
-		String query = "Brutus";
+
+		System.out.println();
+		System.out.println("Term-Dokument-Matrix:");
+		System.out.println("-------------------");
 		TermDokumentMatrix matrix = new TermDokumentMatrix(corpus);
+
+		query = "Brutus";
 		Set<Integer> result = matrix.search(query);
+		System.out.println("Ergebnis für " + query + ": " + result);
 		assertTrue("Mindestens ein Treffer erwartet", result.size() >= 1);
+
+		query = "Caesar";
+		Set<Integer> result1 = matrix.search(query);
+		System.out.println("Ergebnis für " + query + ": " + result1);
+		assertTrue("Mindestens ein Treffer erwartet", result1.size() >= 1);
+
+		query = "Brutus Caesar";
+		Set<Integer> result2 = matrix.search(query);
+		System.out.println("Ergebnis für " + query + ": " + result2);
+		assertTrue("Ergebnis-Set sollte größer sein als bei einzelnem Term",
+				result2.size() >= result.size());
+
+		query = "Brutus Caesar";
+		Set<Integer> result3 = matrix.booleanSearch(query);
+		System.out.println("Ergebnis für " + query + ": " + result3);
+		assertTrue("Ergebnis-Set sollte kleiner sein als bei einzelnem Term",
+				result2.size() >= result.size());
+}
+
+	@Test
+	public void testIndexSearch() {
+		// Testen, ob Suche in invertiertem Index ein Ergebnis liefert:
+
+		System.out.println();
+		System.out.println("Invertierter Index:");
+		System.out.println("-------------------");
+		InvertedIndex index = new InvertedIndex(corpus);
+
+		query = "Brutus";
+		Set<Integer> result = index.search(query);
+		assertTrue("Mindestens ein Treffer erwartet", result.size() >= 1);
+		System.out.println("Ergebnis für " + query + ": " + result);
+
+		query = "Brutus Caesar";
+		Set<Integer> result2 = index.search(query);
+		assertTrue("Ergebnis-Set sollte größer sein als bei einzelnem Term",
+				result2.size() >= result.size());
+		System.out.println("Ergebnis für " + query + ": " + result2);
 	}
 
 	/*
@@ -68,6 +117,7 @@ public class TestBooleanIR {
 	 * "1[56][0-9]{2}\n" ist nur EINE Möglichkeit, die Werke zu unterteilen,
 	 * mögliche Alternativen finden sich unten in der Methode.
 	 */
+	@Ignore
 	@Test
 	public void testRegEx() {
 
