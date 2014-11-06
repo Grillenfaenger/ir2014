ec3ede61d21f0e6703363516f08472d0b90b8346
claesn <c.neuefeind@uni-koeln.de>
2014-11-06 10:25:27 +0100

    Ergänzung der Suche.

--- IR-Praxis/src/ir2/TermDokumentMatrix.java
+++ IR-Praxis/src/ir2/TermDokumentMatrix.java
@@ -2,6 +2,7 @@ package ir2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -15,47 +16,38 @@ import ir1.Corpus;
 public class TermDokumentMatrix implements InformationRetrieval {
 
 	private boolean[][] matrix;
-	/*
-	 * positions hier als Klassenvariable, damit sie für die search-Methode
-	 * genutzt werden können
-	 */
 	private Map<String, Integer> positions;
 
 	public TermDokumentMatrix(Corpus corpus) {
 
-		// Zeitmessung für Vergleich mit linearer Suche:
 		long start = System.currentTimeMillis();
 		System.out.println("Erstelle Matrix ...");
 
 		List<String> works = corpus.getWorks();
 		List<String> terms = getTerms(works);
-		positions = getPositions(terms);// 'Zeilennummern' (s.u.)
+		positions = getPositions(terms);// 'Zeilennummern' der Terme
 		matrix = new boolean[terms.size()][works.size()];
-
+		/*
+		 * Falls beim Erstellen der Matrix der Speicher überläuft, kann man dem
+		 * Prozess mit der Angabe des Parameters "-Xmx256m" (unter Open Run
+		 * Dialog, Arguments, VM Arguments) mehr Speicher zuweisen, hier z.B.
+		 * 256 MB; per default nimmt sich jeder Java-Prozess 64 MB.
+		 */
 		System.out.println("Größe der Matrix: " + terms.size() + " X "
 				+ works.size());
 
 		for (int spalte = 0; spalte < works.size(); spalte++) {
 			String[] tokens = works.get(spalte).split("\\s+");
 			for (int j = 0; j < tokens.length; j++) {
-				String term = tokens[j];// das aktuelle Token
-				int zeile = positions.get(term);// Zeilennummer des Tokens
+				String t = tokens[j];// das aktuelle Token
+				int zeile = positions.get(t);// Zeilennummer des Tokens
 				matrix[zeile][spalte] = true;
 			}
 		}
-
 		System.out.println("Matrix erstellt, Dauer: "
 				+ (System.currentTimeMillis() - start) + " ms.");
 
-		// printMatrix(terms);
-		
-		/*
-		 * Falls beim Erstellen bzw. Ausgeben der Matrix der Speicher
-		 * volllaeuft, kann man den Speicher, den sich der Prozess nimmt, mit
-		 * der Angabe des Parameters "-Xmx256m" (unter Open Run Dialog,
-		 * Arguments, VM Arguments) erhoehen, dies fuer 256 MB; per default
-		 * nimmt sich jeder Java-Prozess 64 MB.
-		 */
+		// printMatrix(terms);// optionale Ausgabe der Matrix
 	}
 
 	/*
@@ -72,8 +64,8 @@ public class TermDokumentMatrix implements InformationRetrieval {
 	/*
 	 * Ermittelt die Terme aller Werke. Das Set wird abschließend in eine Liste
 	 * umgewandelt, da der Listen-Zugriff über get(index) sowohl das Mappen der
-	 * Positionen als auch das ausgeben der Matrix in der printMatrix-Methode
-	 * erleichtert (s.u.)
+	 * Positionen als auch das Ausgeben der Matrix in der printMatrix-Methode
+	 * erleichtert.
 	 */
 	private List<String> getTerms(List<String> works) {
 		Set<String> allTerms = new HashSet<String>();
@@ -101,11 +93,76 @@ public class TermDokumentMatrix implements InformationRetrieval {
 	 */
 	@Override
 	public Set<Integer> search(String query) {
+
+		long start = System.currentTimeMillis();
+		List<String> queries = Arrays.asList(query.split(" "));
 		Set<Integer> result = new HashSet<Integer>();
 
-		// TODO: Matrix-Suche, Boolesche Operatoren
-
+		for (String q : queries) {
+			// erstmal die Zeile ermitteln:
+			Integer zeilennummer = positions.get(q);
+			// und dann Spalte für Spalte (Werk für Werk) nachsehen:
+			for (int i = 0; i < matrix[0].length; i++) {
+				/*
+				 * Bei nicht vorhandenen Wörtern können 'Nullpointer' auftreten.
+				 * Werden diese wie hier explizit vermieden, können an dieser
+				 * Stelle bspw. auch Suchalternativen angezeigt werden
+				 * ("Meinten Sie ...") - was allerdings voraussetzt, dass diese
+				 * zunächst auf Grundlage der Query ermittelt werden. Mehr dazu
+				 * beim Thema "Tolerant Retrieval".
+				 */
+				if (zeilennummer == null) {
+					System.out.println("Term " + q + " nicht gefunden");
+					break;
+				}
+				// die boolesche Matrix enthält ein 'true' für jeden Treffer:
+				if (matrix[zeilennummer][i]) {
+					result.add(i);
+					//Hier behandeln wir die Suchwörter als ODER-verknüpft!
+				}
+			}
+		}
+		System.out.println("Suchdauer: " + (System.currentTimeMillis() - start)
+				+ " ms.");
 		return result;
+	}
+
+	/*
+	 * Alternative Umsetzung der Matrix-Suche unter Verwendung von BitSets.
+	 */
+	public Set<Integer> booleanSearch(String query) {
+
+		long start = System.currentTimeMillis();
+		List<String> queries = Arrays.asList(query.split(" "));
+		Set<Integer> result = new HashSet<Integer>();
+
+		// Wir erstellen ein BitSet für das erste Suchwort (= dessen Zeile):
+		BitSet bitSet = bitSetFor(matrix[positions.get(queries.get(0))]);
+		// ... und dann für alle weiteren Suchwörter:
+		for (String q : queries) {
+			// Die boolschen Operationen bekommen wir nun einfach geschenkt:
+			bitSet.and(bitSetFor(matrix[positions.get(q)]));
+		}
+		// jetzt nur noch die 'true'-Positionen aus resultierendem BitSet holen:
+		for (int i = 0; i < matrix[0].length; i++) {
+			if (bitSet.get(i))
+				result.add(i);
+		}
+		System.out.println("Suchdauer: " + (System.currentTimeMillis() - start)
+				+ " ms.");
+		return result;
+	}
+
+	/*
+	 * Erzeugt ein BitSet aus dem übergebenen boolean[] (bei uns: den Zeilen).
+	 */
+	private BitSet bitSetFor(boolean[] bs) {
+		BitSet set = new BitSet(bs.length);
+		for (int i = 0; i < bs.length; i++) {
+			if (bs[i])
+				set.set(i);
+		}
+		return set;
 	}
 
 }
