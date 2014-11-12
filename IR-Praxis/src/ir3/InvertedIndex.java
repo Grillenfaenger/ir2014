202b2f1cf3f626ed97412017f2b0c16bfeb2a639
claesn <c.neuefeind@uni-koeln.de>
2014-11-12 15:49:08 +0100

    Indexierung und Suche implementiert.

--- IR-Praxis/src/ir3/InvertedIndex.java
+++ IR-Praxis/src/ir3/InvertedIndex.java
@@ -3,11 +3,15 @@ package ir3;
 import ir1.Corpus;
 import ir2.InformationRetrieval;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class InvertedIndex implements InformationRetrieval {
 
@@ -24,23 +28,76 @@ public class InvertedIndex implements InformationRetrieval {
 	}
 
 	private Map<String, SortedSet<Integer>> index(Corpus corpus) {
-		
-		// TODO: Indexierung implementieren!
-		
-		return null;
+		List<String> works = corpus.getWorks();
+		// wir indexieren Werk für Werk:
+		for (int i = 0; i < works.size(); i++) {
+			List<String> terms = PREPROCESSOR.process(works.get(i));
+			for (String t : terms) {
+				// wir holen uns die postings-Liste des terms aus dem Index:
+				SortedSet<Integer> postings = index.get(t);
+				/*
+				 * beim ersten Vorkommen des Terms ist diese noch leer (null),
+				 * also legen wir uns einfach eine neue an:
+				 */
+				if (postings == null) {
+					postings = new TreeSet<>();
+					index.put(t, postings);
+				}
+				/*
+				 * Der Term wird indexiert, indem die Id des aktuellen Werks (=
+				 * der aktuelle Zählerwert) der postings-list hinzugefügt wird:
+				 */
+				postings.add(i);
+			}
+			// printSortedIndexTerms(index);//optionale Ausgabe der Indexterme
+		}
+		return index;
 	}
 
 	@Override
 	public Set<Integer> search(String query) {
 
 		long start = System.currentTimeMillis();
-		Set<Integer> result = new HashSet<Integer>();
-
-		// TODO Suche implementieren!
-
+		// gleicher Preprocessor wie bei Indexierung!
+		List<String> queries = PREPROCESSOR.process(query);
+		/*
+		 * Wir holen uns zunächst die Postings-Listen der Teilqueries:
+		 */
+		List<SortedSet<Integer>> allPostings = new ArrayList<SortedSet<Integer>>();
+		for (String q : queries) {
+			SortedSet<Integer> postings = index.get(q);
+			allPostings.add(postings);
+		}
+		/*
+		 * Damit wir die Effizienz des Algorithmus aus Manning et al erreichen,
+		 * müssen die einzelnen Postings-Listen nach Länge sortiert sein:
+		 */
+		Collections.sort(allPostings, new Comparator<SortedSet<Integer>>() {
+			@Override
+			public int compare(SortedSet<Integer> o1, SortedSet<Integer> o2) {
+				return Integer.valueOf(o1.size()).compareTo(o2.size());
+			}
+		});
+		// Ergebnis ist die Schnittmenge (Intersection) der ersten Liste...
+		SortedSet<Integer> result = allPostings.get(0);
+		// ... mit allen weiteren:
+		for (SortedSet<Integer> set : allPostings) {
+			result = Intersection.of(result, set);
+			// Hier behandeln wir die Suchwörter als UND-Verknüpft!
+		}
 		System.out.println("Suchdauer: " + (System.currentTimeMillis() - start)
 				+ " ms.");
 		return result;
 	}
 
+	/*
+	 * Ausgabe der Indexterme:
+	 */
+	private void printSortedIndexTerms(Map<String, SortedSet<Integer>> index) {
+		TreeSet<String> terms = new TreeSet<>(index.keySet());
+		for (String string : terms) {
+			System.out.println(string);
+		}
+		System.out.println("Anzahl Terme: " + terms.size());
+	}
 }
