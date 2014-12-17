ee02fda17f55f2cc4ea1b5a451fffe39dee10f95
claesn <c.neuefeind@uni-koeln.de>
2014-12-17 15:18:44 +0100

    Nach package ir5 verschoben.

---
+++ IR-Praxis/src/ir5/Ranker.java
@@ -0,0 +1,15 @@
+package ir5;
+
+import java.util.List;
+import java.util.Set;
+
+public class Ranker {
+
+	public Set<Document> rank(Set<Document> result) {
+		
+		// TODO: sortiere das Ergebnis und gebe es (sortiert) zurück
+
+		return null;
+	}
+
+}
