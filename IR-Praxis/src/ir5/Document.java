3d371fb2dd8cb4305ce4722849c570a8873d28b2
claesn <c.neuefeind@uni-koeln.de>
2014-12-17 07:57:59 +0100

    Vorlage für das Seminar.

---
+++ IR-Praxis/src/ir5/Document.java
@@ -0,0 +1,16 @@
+package ir5;
+
+import java.util.Set;
+
+public class Document {
+
+	public Document(String work, String title) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Set<String> getTerms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
