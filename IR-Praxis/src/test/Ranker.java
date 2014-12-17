bbd36e8f96bc00433c02077dacdd58e7b014b0c9
claesn <c.neuefeind@uni-koeln.de>
2014-12-17 15:17:58 +0100

    Klasse für die Sortierung eines Suchergebnisses (Ranking).

---
+++ IR-Praxis/src/test/Ranker.java
@@ -0,0 +1,17 @@
+package test;
+
+import ir5.Document;
+
+import java.util.List;
+import java.util.Set;
+
+public class Ranker {
+
+	public Set<Document> rank(Set<Document> result) {
+		
+		// TODO: sortiere das Ergebnis und gebe es (sortiert) zurück
+
+		return null;
+	}
+
+}
