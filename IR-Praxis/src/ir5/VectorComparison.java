0ada06cb316463c408678e5b3c6570643c7fceb3
claesn <c.neuefeind@uni-koeln.de>
2015-01-05 15:44:40 +0100

    Vorlage für eine Klasse zum Vergleich von Vektoren.

---
+++ IR-Praxis/src/ir5/VectorComparison.java
@@ -0,0 +1,30 @@
+package ir5;
+
+import java.util.List;
+
+public class VectorComparison {
+
+	public static Double compare(List<Double> v1, List<Double> v2) {
+
+		/*
+		 * TODO: Hier erfolgt die Berechnung der Cosinus-Ähnlichkeit auf
+		 * Grundlage von 'dot product' und euklidischer Länge (siehe Folien)
+		 */
+		return null;
+	}
+
+	private static double dotProduct(List<Double> v1, List<Double> v2) {
+		/*
+		 * TODO: Dot product: Summe der Produkte der korrespondierenden Vektor-Werte:
+		 */
+		return 0.0;
+	}
+
+	private static double euclidicLength(List<Double> v) {
+		/*
+		 * TODO: Euklidische Länge: Wurzel aus der Summe der quadrierten Elemente
+		 */
+		return 0.0;
+	}
+
+}
