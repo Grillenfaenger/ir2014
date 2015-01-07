6697ea74db32c97b8b37382acc91a369d6dc16d6
claesn <c.neuefeind@uni-koeln.de>
2015-01-07 14:44:40 +0100

    Berechnung der Cosinus-Ähnlichkeit auf Grundlage von 'dot product' und euklidischer Länge.

--- IR-Praxis/src/ir5/VectorComparison.java
+++ IR-Praxis/src/ir5/VectorComparison.java
@@ -6,24 +6,36 @@ public class VectorComparison {
 
 	public static double compare(List<Double> v1, List<Double> v2) {
 		/*
-		 * TODO: Hier erfolgt die Berechnung der Cosinus-Ähnlichkeit auf
-		 * Grundlage von 'dot product' und euklidischer Länge (siehe Folien)
+		 * Berechnung der Cosinus-Ähnlichkeit auf Grundlage von 'dot product'
+		 * und euklidischer Länge (siehe Folien)
 		 */
-		return 0.0;
+
+		double result = dotProduct(v1, v2)
+				/ (euclidicLength(v1) * euclidicLength(v2));
+
+		return result;
 	}
 
 	private static double dotProduct(List<Double> v1, List<Double> v2) {
 		/*
-		 * TODO: Dot product: Summe der Produkte der korrespondierenden Vektor-Werte:
+		 * Dot product: Summe der Produkte der korrespondierenden Vektor-Werte:
 		 */
-		return 0.0;
+		double sum = 0;
+		for (int i = 0; i < v1.size(); i++) {
+			sum += v1.get(i) * v2.get(i);
+		}
+		return sum;
 	}
 
 	private static double euclidicLength(List<Double> v) {
 		/*
-		 * TODO: Euklidische Länge: Wurzel aus der Summe der quadrierten Elemente
+		 * Euklidische Länge: Wurzel aus der Summe der quadrierten Elemente
 		 */
-		return 0.0;
+		double sum = 0;
+		for (int i = 0; i < v.size(); i++) {
+			sum += Math.pow(v.get(i), 2);
+		}
+		return Math.sqrt(sum);
 	}
 
 }
