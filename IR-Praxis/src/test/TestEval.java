27c9062f6e1ace75e9080f79e0211bb10638cacb
claesn <c.neuefeind@uni-koeln.de>
2015-01-21 14:56:48 +0100

    Tests für Evaluierung gewichteter Ergebnismengen.

--- IR-Praxis/src/test/TestEval.java
+++ IR-Praxis/src/test/TestEval.java
@@ -1,17 +1,20 @@
 package test;
 
+import static org.junit.Assert.*;
 import ir5.Corpus;
 import ir5.Document;
 import ir5.InformationRetrieval;
 import ir5.InvertedIndex;
 import ir5.Ranker;
 import ir6.Evaluation;
+import ir6.EvaluationResult;
 import ir6.GoldStandard;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -32,12 +35,16 @@ public class TestEval {
 	private static List<Document> gold;
 	private static Evaluation evaluation;
 
+	// für Ranking: k und eine "range"
+	int k = 5;
+	private int K_START = 5;
+	private int K_END = 20;
 
 	@BeforeClass
 	public static void setup() {
-		
+
 		/* Zunächst muss alles initialisiert werden */
-		
+
 		query = "king";
 
 		corpus = new Corpus("pg100.txt", "1[56][0-9]{2}\n", "\n");
@@ -74,12 +81,74 @@ public class TestEval {
 
 	@Test
 	public void evalUnranked() {
-		/* TODO: Wir evaluieren zunächst alle Ergebnisse gegen den Goldstandard. */
-		System.out.println();
+		/* Wir evaluieren zunächst alle Ergebnisse gegen den Goldstandard. */
+		EvaluationResult eval = evaluation.evaluate(unranked);
+		assertTrue("Ergebnis sollte größer 0 sein.", eval.f > 0);
+		System.out.println("Unranked, alle: " + eval);
 	}
 
-		/* TODO: Und nun mit Ranking - was müsssen wir beachten? */
+	@Test
+	public void evalRanked() {
+		/* Nun alle gewichteten Ergebnisse gegen den Goldstandard. */
+		EvaluationResult evalRanked = evaluation.evaluate(ranked);
+		assertTrue("Ergebnis sollte größer 0 sein.", evalRanked.f > 0);
+		/*
+		 * Wenn wir das Gesamtergebnis nehmen, spielt das Ranking keine Rolle,
+		 * die Evaluation sollte das gleiche Ergebnis liefern wie ohne Ranking,
+		 * deshalb hier nochmal zum Vergleich:
+		 */
+		EvaluationResult evalUnranked = evaluation.evaluate(unranked);
+		Assert.assertTrue("Ergebnis sollte größer 0 sein.", evalUnranked.f > 0);
+		/*
+		 * Und nun der Vergleich der beiden Ergebnisse - wir vergleichen
+		 * Fließkommazahlen mit einem 'Delta' (zur Präzisionsproblematik von
+		 * Fließkommazahlen, s. z.B. http://en.wikipedia.org/wiki/Floating_point
+		 * #Minimizing_the_effect_of_accuracy_problems):
+		 */
+		Assert.assertEquals(
+				"Bei einer Evaluation über das Gesamtergebnis sollte das Ranking keinen Unterschied machen",
+				evalRanked.f, evalUnranked.f, 1e-9);
+		System.out.println("Ranked, alle: " + evalRanked);
+	}
 
+	@Test
+	public void evalRankedTop() {
+		/* Nun nur die k besten Ergebnisse ... */
+		EvaluationResult evalRanked = evaluation.evaluate(ranked.subList(0, k));
+		EvaluationResult evalUnranked = evaluation.evaluate(unranked.subList(0,
+				k));
+		assertTrue("Evaluation der besten k sollte das Ergebnis verbessern.",
+				evalRanked.f > evalUnranked.f);
+		System.out.println("Ranked, top: " + evalRanked);
+	}
+
+	@Test
+	public void evalRankedMultiK() {
+		/*
+		 * In der Praxis ist es oft nützlich, eine Art Experimentaufbau zu
+		 * definieren, in der Art: 'Probiere alle k von 5 bis 15 und gib die
+		 * Ergebnisse aus' (hier einfach in der Konsole, doch stattdessen könnte
+		 * man das ganze auch tabellarisch in eine Datei schreiben und so
+		 * verschiedene Aufbauten in verschiedenen Files speichern etc.).
+		 */
+		System.out.println("Multiresult:");
+		for (int i = K_START; i < K_END; i++) {
+			EvaluationResult evalRanked = evaluation.evaluate(ranked.subList(0,
+					i));
+			System.out.println(evalRanked + " k=" + i);
+		}
+	}
+
+	@Test
+	public void evalUnrankedMultiK() {
+		System.out.println("Multiresult unranked:");
+		/* Für jedes k von kStart bis kEnd evaluieren und das Ergebnis ausgeben: */
+		for (int i = K_START; i < K_END; i++) {
+			EvaluationResult evalRanked = evaluation.evaluate(unranked.subList(
+					0, i));
+			System.out.println("unranked, top: " + evalRanked + " k=" + i);
+		}
+	}
 
 	/*
 	 * Hilfsmethode für die Ausgabe der Ergebnislisten (inkl. Goldstandard)
